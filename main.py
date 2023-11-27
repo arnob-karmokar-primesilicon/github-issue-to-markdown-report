750297a1311e2dfac6f12586a5c5ed285baff2c6
arnob-karmokar-primesilicon <113158371+arnob-karmokar-primesilicon@users.noreply.github.com>
2023-11-27 09:44:45 +0600

    Added new project status

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ GITHUB_PERSONAL_ACCESS_TOKEN = sys.argv[4]
 # Define a custom sorting key function
 def custom_sort_key(issue):
     # Define the order of project statuses
-    status_order = {"Open": 1, "Reopened": 2, "In Progress": 3, "Done": 4}
+    status_order = {"Open": 1, "Reopened": 2, "In Progress": 3, "Need Review": 4, "Review In Progress": 5,"Done": 6}
     
     # Return a tuple for sorting, with project_status being the key
     return (status_order.get(issue.project_status, 5), issue.complete_date,issue.deadline)
@@ -174,7 +174,7 @@ if response.status_code == 200:
     ## Filtering Issues
     filtered_issue_list = [issue for issue in issue_list if
                       (issue.complete_date in date_list) or
-                      (issue.project_status in ["In Progress", "Open", "Reopened"])]
+                      (issue.project_status in ["In Progress", "Open", "Reopened","Review In Progress","Need Review"])]
     
     # Sort the filtered list using the custom sorting key
     sorted_issue_list = sorted(filtered_issue_list, key=custom_sort_key)
