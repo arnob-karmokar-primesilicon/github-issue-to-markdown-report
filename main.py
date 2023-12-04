1249f9eede0ee100e7ccf6b3345bae44953b48b7
arnob-karmokar-primesilicon <113158371+arnob-karmokar-primesilicon@users.noreply.github.com>
2023-12-04 10:14:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ def custom_sort_key(issue):
     # Define the order of project statuses
     status_order = {"Open": 1, "Reopened": 2, "In Progress": 3, "Need Review": 4, "Review In Progress": 5, "Done": 6, "Paused": 7}
     print(issue)
+     # Assign a default value for sorting if complete_date or deadline is None
+    default_complete_date = issue.complete_date or "9999-99-99"
+    default_deadline = issue.deadline or "9999-99-99"
+   
     # Return a tuple for sorting, with project_status being the key
-    return (status_order.get(issue.project_status, 8), issue.complete_date,issue.deadline,issue.title)
+    return (status_order.get(issue.project_status, 8), default_complete_date, default_deadline)
 
 
 # Define your GraphQL query
